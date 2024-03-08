923197c519e822f6d08f210a2fb52615e065d45a
Charles Sampson <csampson@qualtrics.com>
2024-03-08 09:31:52 -0700

    better formatting for summary

--- context.go
+++ context.go
@@ -1,14 +1,12 @@
 package ctxclient
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"math"
 	"net/http"
-	"os"
 	"sort"
 	"strings"
 	"time"
@@ -237,9 +235,7 @@ func (ctxClient *ContextClient) CloseContext(contextId string) (string, error) {
 	// return "new contextId", nil
 }
 
-func (ctxClient *ContextClient) ListFormattedContexts(filterParams QSParams) (map[string]FormattedContext, error) {
-	// formatted := map[string]FormattedContext{}
-	// listed := map[string]Context{}
+func (ctxClient *ContextClient) ListFormattedContexts(filterParams QSParams) ([]FormattedContext, error) {
 	cs, err := ctxClient.ListContexts(filterParams)
 	ordered := *cs
 	if err != nil {
@@ -250,240 +246,122 @@ func (ctxClient *ContextClient) ListFormattedContexts(filterParams QSParams) (ma
 		return ordered[i].Created > ordered[j].Created
 	})
 
-	fmt.Println("ordered")
-	// for _, c := range ordered {
-	// 	fmt.Printf("created: %s\n", c.Created)
-	// 	listed[c.ContextId] = c
-	// }
-
 	formatted := formatContexts(ordered)
-
-	keys := make([]string, 0, len(formatted))
-	for k := range formatted {
-		keys = append(keys, k)
-	}
-	sorted := []FormattedContext{}
-
-	for _, key := range keys {
-		sorted = append(sorted, formatted[key])
-	}
-
-	sort.Slice(sorted, func(i, j int) bool {
-		return sorted[i].Created > sorted[j].Created
-	})
-
-	out, err := json.MarshalIndent(formatted, "", "  ")
-	if err != nil {
-		return nil, errors.New(fmt.Sprintf("error marshaling contexts to JSON: %s", err))
-	}
-	fmt.Printf("out:\n%+v\n----\n", string(out))
-
 	return formatted, nil
 }
 
-func formatContexts(ordered []Context) map[string]FormattedContext {
+func formatContexts(ordered []Context) []FormattedContext {
+	type ctxid struct {
+		Name     string `json:"name,omitempty"`
+		ParentId string `json:"parentId,omitempty"`
+	}
+	type newOrder struct {
+		CID     ctxid
+		Context FormattedContext
+	}
 	lookup := map[string]Context{}
+	flattenedFormatted := map[ctxid][]FormattedContext{}
+	consolidatedFormatted := map[ctxid]FormattedContext{}
 	for _, c := range ordered {
-		fmt.Printf("created: %s\n", c.Created)
 		lookup[c.ContextId] = c
-	}
-	// search range
-	// 2024-02-29T01:16:54Z
-	// 2024-02-29T01:22:22Z
-	parentLookup := map[string]Context{}
-	parentalGroups := map[string][]FormattedContext{}
-	// formatted := map[string]*FormattedContext{}
-	output := map[string]FormattedContext{}
-	scn := bufio.NewScanner(os.Stdin)
-
-	for i := len(ordered) - 1; i >= 0; i-- {
-		c := ordered[i]
-		nameWithParentId := fmt.Sprintf("%s(%s)", c.Name, c.ParentId)
-		cTime := getTimeDiff(c.Created, c.Completed)
-		cNotes := []string{}
-		if string(c.Notes) != "" {
-			err := json.Unmarshal(c.Notes, &cNotes)
-			if err != nil {
-				panic(fmt.Sprintf("error in unmarshal c.Notes: %s\njson: %s\n", err.Error(), string(c.Notes)))
-			}
+		cID := ctxid{
+			Name:     c.Name,
+			ParentId: c.ParentId,
 		}
-		ogid, inLookup := parentLookup[nameWithParentId]
-		if inLookup {
-			o := lookup[ogid.ContextId]
-			createdTime := getTimeDiff(o.Created, o.Completed)
-			fmt.Printf("c time spent\n%+v\n", cTime)
-			fmt.Printf("o time spent\n%+v\n----\n", createdTime)
-			oNotes := []string{}
-			// combinedNotes := json.RawMessage{}
-			if string(o.Notes) != "" {
-				err := json.Unmarshal(o.Notes, &oNotes)
-				if err != nil {
-					panic(fmt.Sprintf("error in unmarshal combined.Notes: %s\njson: %s\n", err.Error(), string(o.Notes)))
-				}
-			}
-
-			noteSlice := append(cNotes, oNotes...)
-			fmt.Printf("new note slice\n%v\n----\n", noteSlice)
-			combinedNotes, err := json.Marshal(noteSlice)
-			if err != nil {
-				panic(err)
-			}
-
-			if c.Created > o.Created {
-				c.Created = o.Created
-			}
-			if c.Completed < o.Completed {
-				c.Completed = o.Completed
-			}
-			fmt.Printf("notes to combine\nc\n%+v\no\n%+v\n----\n", c.Notes, o.Notes)
-			c.Notes = combinedNotes
-			o.Notes = combinedNotes
-		}
-	}
-
-	l, _ := json.MarshalIndent(lookup, "", "  ")
-	fmt.Printf("lookup: %s\n", string(l))
-
-	fmt.Printf("parentLookup: %+v\n", parentLookup)
-	pLook, _ := json.MarshalIndent(parentLookup, "", "  ")
-	fmt.Println(string(pLook))
-
-	for _, c := range ordered {
-		if c.ParentId != "" {
-			p := lookup[c.ParentId]
-			pid := fmt.Sprintf("%s(%s)", p.Name, p.ParentId)
-			parent := parentLookup[pid]
-			fmt.Printf("on %s\nwith: %s\nold: %s\nnew: %s\n", c.Name, pid, c.ParentId, parent.ContextId)
-			// c.ParentId = parentId
-		}
+		existingFormatted := flattenedFormatted[cID]
 		f := formatContext(&c)
-		if c.ParentId != "" {
-			parentalGroups[c.ParentId] = append(parentalGroups[c.ParentId], f)
-		}
+		existingFormatted = append(existingFormatted, f)
+		sort.Slice(existingFormatted, func(i, j int) bool {
+			return existingFormatted[i].Created < existingFormatted[j].Created
+		})
+		flattenedFormatted[cID] = existingFormatted
 	}
 
-	fmt.Println("parentalGroups:")
-	for id, children := range parentalGroups {
-		fmt.Println(id)
-		for _, c := range children {
-			fmt.Printf("    %s\n", c.Name)
+	theNewOrder := []newOrder{}
+	for cID, l := range flattenedFormatted {
+		f := FormattedContext{}
+		for _, c := range l {
+			c.TimeSpent = getTimeDiff(c.Created, c.Completed)
+			if f.Name != "" {
+				// add times together
+				if f.TimeSpent.Unit == c.TimeSpent.Unit {
+					f.TimeSpent.Time += c.TimeSpent.Time
+				} else {
+					// balance units and then add
+					fmt.Printf("unbalanced units with self: %s and %s\n", f.TimeSpent.Unit, c.TimeSpent.Unit)
+				}
+				f.Completed = c.Completed
+				fNotes := []string{}
+				cNotes := []string{}
+				if f.Notes != nil {
+					err := json.Unmarshal(f.Notes, &fNotes)
+					if err != nil {
+						panic("error in unmarshal f.Notes: " + err.Error())
+					}
+				}
+				if c.Notes != nil {
+					err := json.Unmarshal(c.Notes, &cNotes)
+					if err != nil {
+						panic("error in unmarshal c.Notes: " + err.Error())
+					}
+				}
+				combinedNotes := append(fNotes, cNotes...)
+				if len(combinedNotes) > 0 {
+					noteBytes, err := json.Marshal(combinedNotes)
+					if err != nil {
+						panic("error in marshal combinedNotes: " + err.Error())
+					}
+					f.Notes = noteBytes
+				}
+			} else {
+				f = c
+			}
 		}
+		consolidatedFormatted[cID] = f
+		theNewOrder = append(theNewOrder, newOrder{
+			CID:     cID,
+			Context: f,
+		})
 	}
 
-	// TODO: resolve children and append to parents in formatted
+	sort.Slice(theNewOrder, func(i, j int) bool {
+		return theNewOrder[i].Context.Created > theNewOrder[j].Context.Created
+	})
 
-	// for _, c := range list {
-	// 	existing := formatted[c.ContextId]
-	// 	existingWithName := formatted[c.Name]
-	// 	fmt.Printf("\non name: %s id: %s\n", c.Name, c.ContextId)
-	// 	fmt.Printf("%+v\n", c)
-	// 	fmt.Printf("existing: %+v\n", existing)
-	// 	fmt.Printf("existingWithName: %+v\n", existingWithName)
-	// 	f := FormattedContext{}
-	// 	if c.ParentId == "" {
-	// 		fmt.Println("no parent")
-	// 		if existing == nil && existingWithName == nil {
-	// 			fmt.Println("no children")
-	// 			f = formatContext(&c)
-	// 			formatted[c.Name] = &f
-	// 		}
-	// 	} else {
-	// 		p := formatted[c.ParentId]
-	// 		if p == nil {
-	// 			fmt.Println("has parent but it hasn't been defined yet")
-	// 			p = &FormattedContext{
-	// 				ContextId:   c.ParentId,
-	// 				SubContexts: []FormattedContext{formatContext(&c)},
-	// 			}
-	// 			fmt.Printf("add parent to map uder id:\n%+v\n", p)
-	// 			formatted[c.ParentId] = p
-	// 		} else {
-	// 			fmt.Println("has parent and it has been defined")
-	// 			f := formatContext(&c)
-	// 			p.SubContexts = append(p.SubContexts, f)
-	// 			sort.Slice(p.SubContexts, func(i, j int) bool {
-	// 				return p.SubContexts[i].Created < p.SubContexts[j].Created
-	// 			})
-	// 			fmt.Printf("update parent with new child:\n%+v\n", p)
-	// 		}
-	// 		// formatted[c.ContextId] = &f
-	// 	}
+	formatted := []FormattedContext{}
+	for _, ctx := range theNewOrder {
+		cID := ctx.CID
+		c := consolidatedFormatted[cID]
+		p := lookup[cID.ParentId]
+		if p.Name != "" {
+			pID := ctxid{
+				Name:     p.Name,
+				ParentId: p.ParentId,
+			}
+			parent := consolidatedFormatted[pID]
 
-	scn.Scan()
-	// if existing != nil {
-	// 	if existingWithName != nil {
-	// 		fmt.Println("existingWithName")
-	// 	} else {
-	// 		fmt.Println("existing by id only")
-	// 		f = formatContext(&c)
-	// 		f.SubContexts = append(f.SubContexts, existing.SubContexts...)
-	// 		sort.Slice(f.SubContexts, func(i, j int) bool {
-	// 			return f.SubContexts[i].Created < f.SubContexts[j].Created
-	// 		})
-	// 		fmt.Printf("f: %+v\n", f)
-	// 		delete(formatted, c.ContextId)
-	// 	}
-	// }
-	// if c.ParentId == "" {
-
-	// 	formatted[c.ContextId] = &f
-	// } else {
-	// 	p := formatted[c.ParentId]
-	// 	if p == nil {
-	// 		p = &FormattedContext{
-	// 			ContextId:   c.ParentId,
-	// 			SubContexts: []FormattedContext{f},
-	// 		}
-	// 	} else {
-	// 		p.SubContexts = append(p.SubContexts, f)
-	// 		sort.Slice(p.SubContexts, func(i, j int) bool {
-	// 			return p.SubContexts[i].Created > p.SubContexts[j].Created
-	// 		})
-	// 	}
-	// 	formatted[c.ParentId] = p
-	// }
-	// if existing != nil {
-	// 	f.SubContexts = append(f.SubContexts, existing.SubContexts...)
-	// } else {
-	// 	f = formatContext(&c)
-	// }
-	// if c.ParentId == "" {
-	// 	formatted[c.ContextId] = &f
-	// } else {
-	// 	p := formatted[c.ParentId]
-	// 	if p == nil {
-	// 		p = &FormattedContext{
-	// 			ContextId:   c.ParentId,
-	// 			SubContexts: []FormattedContext{f},
-	// 		}
-	// 	} else {
-	// 		p.SubContexts = append(p.SubContexts, f)
-	// 		sort.Slice(p.SubContexts, func(i, j int) bool {
-	// 			return p.SubContexts[i].Created > p.SubContexts[j].Created
-	// 		})
-	// 	}
-	// 	formatted[c.ParentId] = p
-	// }
-	// }
-
-	// for id, c := range formatted {
-	// 	output[id] = *c
-	// }
-
-	// for id, c := range cs {
-	// 	if c.ParentId != "" {
-	// 		f := formatContext(&c)
-	// 		p := formatted[c.ParentId]
-	// 		p.SubContexts = append(p.SubContexts, f)
-	// 		sort.Slice(p.SubContexts, func(i, j int) bool {
-	// 			return p.SubContexts[i].Created < p.SubContexts[j].Created
-	// 		})
-	// 		formatted[c.ParentId] = p
-	// 	} else {
-	// 		formatted[id] = formatContext(&c)
-	// 	}
-	// }
-	return output
+			kids := parent.SubContexts
+			kids = append(kids, c)
+			sort.Slice(kids, func(i, j int) bool {
+				return kids[i].Created < kids[j].Created
+			})
+			if parent.TimeSpent.Unit == c.TimeSpent.Unit {
+				parent.TimeSpent.Time += c.TimeSpent.Time
+				parent.TimeSpent.Time = math.Round(parent.TimeSpent.Time*100) / 100
+			} else {
+				// balance units and then add
+				fmt.Printf("unbalanced units with parent: %s and %s\n", parent.TimeSpent.Unit, c.TimeSpent.Unit)
+			}
+			parent.SubContexts = kids
+			consolidatedFormatted[pID] = parent
+		} else {
+			formatted = append(formatted, c)
+		}
+	}
+	sort.Slice(formatted, func(i, j int) bool {
+		return formatted[i].Created < formatted[j].Created
+	})
+	return formatted
 }
 
 func formatContext(c *Context) FormattedContext {
@@ -492,6 +370,7 @@ func formatContext(c *Context) FormattedContext {
 		Notes:     c.Notes,
 		ContextId: c.ContextId,
 		Created:   c.Created,
+		Completed: c.Completed,
 	}
 }
 
